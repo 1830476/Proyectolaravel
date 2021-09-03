55cc17d58972564d42421d66e16b081816365102
1830476 <1830476@upv.edu.mx>
2021-09-02 21:18:36 -0500

    Terminado

---
+++ frontend/src/app/movie/movie.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from '../models/Movie';
+import { MoviesService } from '../services/movies.service';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent implements OnInit {
+  public movie$: Promise<Movie> | undefined;
+  constructor(
+    private moviesService : MoviesService,
+    private activateRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+        this.getMovie();
+  }
+
+  getMovie = async() =>{
+    let routePramId: string | number | null = this.activateRoute.snapshot!.paramMap.get("id");
+    if(routePramId){
+      routePramId = parseInt(routePramId);
+      this.movie$ = this.moviesService.getMovieById(routePramId);
+    }
+  }
+
+}
